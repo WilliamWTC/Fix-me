104f97730fc39d67b065094d1960afff5a27c118
williamWTC <williammemor@gmail.com>
2021-01-04 23:03:51 +0200

    ...

--- Broker/src/main/java/broker/Main.java
+++ Broker/src/main/java/broker/Main.java
@@ -1,3 +1,11 @@
-public class Main {
-    
+package broker;
+import javax.swing.*;
+import static java.lang.Integer.parseInt;
+import java.io.*;
+import java.net.Socket;
+
+public class Main
+{
+    private static final int PORT;
+    private static final String ADDRESS;
 }
