6eb7bb99eb2fe37457f84868dc76b1ab7f9e0b9b
williamWTC <williammemor@gmail.com>
2021-01-05 07:28:12 +0200

    ...

--- Broker/src/main/java/broker/Main.java
+++ Broker/src/main/java/broker/Main.java
@@ -8,4 +8,41 @@ public class Main
 {
     private static final int PORT;
     private static final String ADDRESS;
+    String id;
+    GrapeVine brokerMsg;
+    String[] shares = { "S&P500", "NASDAQ"};
+
+    JComboBox<String> equity = new JComboBox<>(shares);
+    JFrame frame = new JFrame("EasyEquities");
+    JButton btnBuy = new JButton("Buy");
+    JButton btnSell = new JButton("Sell");
+    JTextField Equity   = new JTextField(25);
+    JTextField nshare = new JTextField(5);
+    JTextArea messageArea = new JTextArea(5, 5);
+
+    public Main()
+    {
+        txtEquity.setEditable(true);
+        messageArea.setEditable(false);
+        frame.getContentPane().add(equity, "North");
+        frame.getContentPane().add(nshare, "Center");
+        frame.getContentPane().add(new JScrollPane(messageArea), "South");
+        frame.getContentPane().add(btnBuy, "West");
+        frame.getContentPane().add(btnSell, "East");
+        frame.pack();
+
+        btnBuy.addActionListener( e -> {
+            String message ="";
+            message = "Id=" + this.id + "|"+ "Order-type=BUY|" + "Equity=" + ( String )equity.getSelectedItem()  + "|" +  "Shares=" + shares.getText() + "|";
+            message = sign(message);
+            out.println(message);
+        });
+
+        btnSell.addActionListener( e -> {
+            String message ="";
+            message = "Id=" + this.id + "|"+ "Order-type=SELL|" + "Equity=" + ( String )equity.getSelectedItem() + "|" +  "Shares=" + shares.getText() + "|";
+            message = sign(message);
+            out.println(message);
+        });
+    }
 }
